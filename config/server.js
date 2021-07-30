e9f994a778496c0e1a6f28c2e823f6b11616b6ba
Gareth Hind <garethjhind@gmail.com>
2021-07-30 19:37:30 +1000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6a45e610a66f67464c89be92f286c2f2'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '96523ca6-25c4-4dfe-bfd4-79ea8cfd0cc5'
+};
